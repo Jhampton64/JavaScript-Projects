e6f98dad1cb985f8ee1af9945345eee25c12ab5e
Jhampton64 <63685850+Jhampton64@users.noreply.github.com>
2020-05-04 01:32:29 -0500

    JavaScript pg.58
    JavaScript assignment for pg.58

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,18 @@
+window.alert("hello world!");
+document.write('Hello, World!');
+var A="This is a string";
+document.write("Kia told Max,\"Hurry it up,the moonlight fading!\"<br> \'Just hold on you vampire!\"Max responed.");
+document.write("\"Do what you feel you must do,"+"because I don\'t hide in the day\""+"-random ,vampire");
+document.write(A);
+
+
+var Family="The Parkers", Dad="Richard",Mom="Mary",Uncle="Ben",Aunt="May",Son="Peter";
+var Family=Family.fontcolor("purple");
+var Dad=Dad.fontcolor("Red");
+var Mom=Mom.fontcolor("Blue");
+var Uncle=Uncle.fontcolor("Yellow");
+var Aunt=Aunt.fontcolor("Pink");
+var Son=Son.fontcolor("Silver");
+document.write(Son)
+
+document.write (9+9);
