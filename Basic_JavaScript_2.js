64bf3d2bf0f6ddcbf672e6dc3709f4dd46aecc54
Jhampton64 <63685850+Jhampton64@users.noreply.github.com>
2020-05-04 20:59:02 -0500

    JAVASCRIPT COURSE pg63

---
+++ Basic_JavaScript_2.js
@@ -0,0 +1,11 @@
+function My_First_Function() {
+    var str="Let's see if this works.";
+    var result=str.fontcolor("blue");
+    document.getElementById("blue_text").innerHTML=result;
+}
+
+function myFunction() {
+    var sentence= "I still learning,";
+    sentence +="a lot from this textbook!";
+    document.getElementById("idea").innerHTML=sentence;
+}
